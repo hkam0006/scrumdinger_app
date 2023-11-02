310a8ec0c3c23ec5fc66cca014c5497115991663
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2023-11-02 18:27:21 +1100

    Created routing for pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/js/NavBar';
+import Login from './pages/js/LandingPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return( 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<NavBar />} />
+      </Routes>
+    </BrowserRouter>
+    );
 }
 
 export default App;
