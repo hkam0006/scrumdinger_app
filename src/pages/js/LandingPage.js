7fcf9a9827e7100447ee0db455aef501188bac96
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2023-11-06 03:10:11 +1100

    Added page redirection if user is logged in

--- src/pages/js/LandingPage.js
+++ src/pages/js/LandingPage.js
@@ -3,10 +3,18 @@ import SignIn from '../../components/js/SignIn'
 import { FaPersonRunning } from "react-icons/fa6";
 import { useState } from 'react';
 import SignUp from '../../components/js/SignUp';
+import {Navigate} from "react-router-dom"
+import { useAuthContext } from '../../hooks/AuthContext';
+
 
 export default function LandingPage(){
   const [loginState, setLoginState] = useState(true)
-
+  const {currentUser} = useAuthContext()
+  
+  if (currentUser){
+    return <Navigate to="/home" />
+  }
+  
   return (
     <>
     <div className="page">
