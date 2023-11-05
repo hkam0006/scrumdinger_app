e9a7d905648dfce7c9c667b175b1a669882bc0be
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2023-11-06 03:09:25 +1100

    Added Profile Page

--- src/components/js/Profile.jsx
+++ src/components/js/Profile.jsx
@@ -1,9 +1,12 @@
 import { useNavigate } from 'react-router-dom';
 import { useAuthContext } from '../../hooks/AuthContext'
 import '../css/Profile.css'
+import { useState } from 'react';
+import PasswordModal from './PasswordModal';
 
 export default function Profile() {
-  const { authorisation, deleteAccount } = useAuthContext()
+  const { currentUser, deleteAccount, changePassword } = useAuthContext()
+  const [modal, setModal] = useState(false)
   const navigate = useNavigate()
 
   function deleteButtonPressed() {
@@ -12,7 +15,12 @@ export default function Profile() {
     });
   }
 
+  function toggleShowModal() {
+    setModal(!modal)
+  }
+
   return <>
+    {modal ? <PasswordModal toggleShow={() => toggleShowModal()} /> : <></>}
     <div className='container'>
       <div className='content'>
         <div className='sidebar'>
@@ -25,10 +33,12 @@ export default function Profile() {
         <div className='main-screen'>
           <h1>My Account</h1>
           <div className='AccountInformation'>
-            <label>First Name: <input type='text'></input></label>
+            <label>First Name:<input type='text'></input></label>
             <label>Last Name: <input type='text'></input></label>
-            <label>Email: <input type='email' value={authorisation.user.email} disabled={true} /></label>
-            <button className='ChangePasswordButton'>Change Password</button>
+            <label>Email: <input type='email' /></label>
+          </div>
+          <div className='BottomGrid'>
+            <button className='ChangePasswordButton' onClick={() => toggleShowModal()}>Change Password</button>
             <button className='DeleteButton' onClick={() => deleteButtonPressed()}>Delete Account</button>
           </div>
         </div>
