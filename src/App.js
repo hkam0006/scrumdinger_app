ced661ea4f5d8521a54114538a34fa635b0858a6
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2023-11-12 13:28:29 +1100

    Added routes to sub routes to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import NavBar from './components/js/NavBar';
 import RequireAuth from './components/js/RequireAuth';
 import { AuthContextProvider } from './hooks/AuthContext';
 import HomePage from './pages/js/HomePage';
 import LandingPage from './pages/js/LandingPage';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SprintPage from './pages/js/SprintPage';
+import ChatPage from './pages/js/ChatPage';
+import TeamPage from './pages/js/TeamPage';
+import Profile from './components/js/Profile';
+import MetricsPage from './pages/js/MetricPage';
 
 function App() {
   return( 
@@ -13,7 +17,13 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route element={<RequireAuth />}>
-            <Route path="/home" element={<HomePage />} />
+            <Route path="/home" element={<HomePage />}>
+              <Route path='team' element={<TeamPage/>}/>
+              <Route path='sprint' element={<SprintPage/>}/>
+              <Route path='metrics' element={<MetricsPage/>}/>
+              <Route path='chat' element={<ChatPage/>}/>
+              <Route path='profile' element={<Profile/>}/>
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
