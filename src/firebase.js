e72d609452d6feb355ac5b557615fe3bd3edc422
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2023-11-12 13:28:43 +1100

    Removed comment

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth"
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
